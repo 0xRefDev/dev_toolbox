413bc5f2f11e6b9db0c86809bb847d4947ccbdb0
0xRefDev <refh4ck.dev@gmail.com>
2026-01-12 14:36:03 -0400

    feat: add chevron arrows with two directions

---
+++ src/icons/ChevronL.jsx
@@ -0,0 +1,3 @@
+export function ChevronL(props) {
+  return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m15 6l-6 6l6 6"></path></svg>);
+}
---
+++ src/icons/ChevronR.jsx
@@ -0,0 +1,3 @@
+export function ChevronR(props) {
+  return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m9 6l6 6l-6 6"></path></svg>);
+}
