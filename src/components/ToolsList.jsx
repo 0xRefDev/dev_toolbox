22679cf485bab0e3c60d325d4e94e89299a47f69
0xRefDev <refh4ck.dev@gmail.com>
2026-01-13 15:00:47 -0400

    fix: change the grid height

--- src/components/ToolsList.jsx
+++ src/components/ToolsList.jsx
@@ -65,7 +65,7 @@ export function ToolsList({ allTools }) {
         ))}
       </div>
 
-      <article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-3 p-8 min-h-[600px]">
+      <article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-3 p-8 ">
         {currentTools.map((tool, index) => (
           <ToolCardReact
             key={`${tool.name}-${index}`}
